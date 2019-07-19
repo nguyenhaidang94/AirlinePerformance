1ba3c21d9a842a0188bf24af98642c92cf24b2c2
dang <nguyenhaidang.uit@gmail.com>
2019-07-19 16:43:57 +0700

    draw airports

--- draw.js
+++ draw.js
@@ -19,20 +19,24 @@ d3.json("us-states.json").then(function(json) {
 		.attr("class", "state");
 });
 
-var originGeo = [-117.383003235, 34.597499847399995]
-var destGeo = [-73.87259674, 40.77719879]
 var airport = svg.append("g");
-airport.selectAll("circleOrigin")
-	.data([originGeo, destGeo])
-	.enter()
-	.append("circle")
-	.attr('cx', function (d) { return projection(d)[0]; })
-	.attr('cy', function (d) { return projection(d)[1]; })
-	.attr('r', '3px')
-	.attr('class', 'airport');
+d3.csv("delayed_airports.csv").then(function(data){
+	airport.selectAll("circleOrigin")
+		.data(data)
+		.enter()
+		.append("circle")
+		.attr('cx', function (item) { return projection([item.origin_long, item.origin_lat])[0]; })
+		.attr('cy', function (item) { return projection([item.origin_long, item.origin_lat])[1]; })
+		.attr('name', function (item) { return item.ORIGIN;})
+		.attr('r', '5px')
+		.attr('class', 'airport');
+});
 
+
+/*
 var route = svg.append("g");
 route.append("path")
 	.datum({type: "LineString", coordinates: [originGeo, destGeo]})
 	.attr("d", path)
 	.attr("class", "route");
+*/
