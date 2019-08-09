6a35d2d3048541921338cf3e43243c1c2d151cb8
caokyhieu <caokyhieu253@gmail.com>
2019-08-09 16:17:56 +0700

    add button to barchart

--- static/bar_group.js
+++ static/bar_group.js
@@ -1,20 +1,70 @@
+// jquery for control
+    $( function() {
+       // run the currently selected effect
+       function redraw() {
+         // get effect type from
+         var selectedChart = $( "#barchart" ).val();
 
-// var data;
-//   d3.json("http://127.0.0.1:5000/static/groupbarchart.json", function(dataFromServer) {
-//       data = dataFromServer;
-//     });
+         // Most effect types need no options passed by default
+         var link_data = "http://127.0.0.1:5000/static/groupbarchart.json";
+         // some effects have required parameters
+         if ( selectedChart === "CARRIER" ) {
+           link_data = "http://127.0.0.1:5000/static/groupbarchart.json";
+         } else if ( selectedChart === "AIRPORT" ) {
+           link_data = "http://127.0.0.1:5000/static/groupbarchart.json";
+         }
+         else if (selectedChart === "CITY"){
+           link_data = "http://127.0.0.1:5000/static/groupbarchart.json";
+         }
 
-    d3.json("http://127.0.0.1:5000/static/groupbarchart.json", function(d) {
-      return {
-            DAY_OF_WEEK: d.DAY_OF_WEEK,
-            OP_UNIQUE_CARRIER: d.OP_UNIQUE_CARRIER,
-            delay_time: +d.delay_time,
-            type :d.type
-      };
-    }).then(function(rows) {
-      console.log(rows);
-      render(rows);
-    });
+         // Remove svg
+         var div = document.getElementById("mydiv");
+         var remove_children = div.childNodes;
+         //remove the current chart
+         div.removeChild(remove_children[remove_children.length -1]);
+         // redraw
+         draw(link_data);
+
+       };
+
+       // Redraw when hit button
+       $( "#button" ).on( "click", function() {
+         redraw();
+       });
+
+     } );
+
+
+
+    function draw(link){
+        d3.json(link, function(d) {
+          return {
+                DAY_OF_WEEK: d.DAY_OF_WEEK,
+                OP_UNIQUE_CARRIER: d.OP_UNIQUE_CARRIER,
+                delay_time: +d.delay_time,
+                type :d.type
+          };
+        }).then(function(rows) {
+          console.log(rows);
+          render(rows);
+        });
+      }
+
+    draw("http://127.0.0.1:5000/static/groupbarchart.json");
+    function displayTooltip(item){
+    	tooltip.transition().duration(200);
+    	tooltip.html(
+    				 "Delay time: " + item.delay_time + " minutes"
+    				)
+    		.style("opacity", 0.9)
+    		.style("left", (d3.event.pageX - 10) + "px")
+    		.style("top", (d3.event.pageY - 64) + "px");
+    }
+
+    function hideTooltip(){
+    	tooltip.transition().duration(200)
+    		.style("opacity", 0);
+    }
 
     function render(data) {
 
@@ -22,6 +72,10 @@
           width = 960 - margin.left - margin.right,
           height = 500 - margin.top - margin.bottom;
 
+
+      var tooltip = d3.select("div#mydiv").append("div")
+      	.attr("class", "tooltip")
+      	.style("opacity", 0);
 
       var x0 =  d3.scaleBand()
           .rangeRound([0, width])
@@ -101,16 +155,19 @@
                                         .attr("height", function(d) { return height - y(2);})
                                         .on("mouseover", function(d) {
                                             d3.select(this).style("fill", d3.rgb(color(d.type)).darker(2));
+                                             displayTooltip(d);
                                                 })
                                         .on("mouseout", function(d) {
                                                 d3.select(this).style("fill", color(d.type));
+                                                hideTooltip() ;
                                                 }))
                 .call((parent)=>parent.append("text")
                 .text(function(d){return d.OP_UNIQUE_CARRIER})
                 .attr("x", function(d, i) {
                   return x1(d.type) + 20;
                   })
-                  );
+                  )
+                  ;
             slice.selectAll("text")
                   .attr("y", function(d) { return y(d.delay_time) - 5 ; });
 
@@ -120,7 +177,8 @@
                 .delay(function (d) {return Math.random()*1000;})
                 .duration(1000)
                 .attr("y", function(d) { return y(d.delay_time); })
-                .attr("height", function(d) { return height - y(d.delay_time); });
+                .attr("height", function(d) { return height - y(d.delay_time); })
+                ;
 
 
 
--- static/sunburst.js
+++ static/sunburst.js
@@ -1,6 +1,6 @@
 d3.json("http://127.0.0.1:5000/static/sunbusrt_data.json", function(d) {
   return {
-        DAY_OF_WEEK: d.DAY_OF_WEEK,
+        DAY_OF_WEEK:  d.DAY_OF_WEEK,
         OP_UNIQUE_CARRIER: d.OP_UNIQUE_CARRIER,
         count: +d.count,
         Node :d.Node
@@ -10,12 +10,13 @@ d3.json("http://127.0.0.1:5000/static/sunbusrt_data.json", function(d) {
   const myChart = Sunburst();
   var data1 = rows;
   data1 = _.nest(data1, ["Node","DAY_OF_WEEK","OP_UNIQUE_CARRIER"],function(d){return d[0].count});
+  console.log(data1);
   const color = d3.scaleOrdinal(d3.schemePaired);
   myChart.data(data1)
   .size('value')
   .color((d, parent) => color(parent ? parent.data.name : null))
+  .showLabels(true)
+  .excludeRoot(false)
   .tooltipContent((d, node) => `Number of Delay: <i>${node.value}</i>`)
   (document.getElementById('sunburst'));
-
-
 });
