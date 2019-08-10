81f5c20569c324dd0f8745cbb99b2a3e2c167201
dang <nguyenhaidang.uit@gmail.com>
2019-08-10 10:20:35 +0700

    use select2 for airport selection

--- static/draw.js
+++ static/draw.js
@@ -23,6 +23,50 @@ var tooltip = d3.select("div#map").append("div")
 	.attr("class", "tooltip")
 	.style("opacity", 0);
 
+$("#airportSelection").change(function(){
+	var airportCode = $("#airportSelection").val();
+	if (airportCode != ""){
+		airportGraph.select("circle[name='" + airportCode + "']").dispatch("click");
+	}
+});
+
+var airportGraph = svg.append("g");
+var routeGraph = svg.append("g");
+d3.csv("http://127.0.0.1:5000/static/origin_delay.csv").then(function(data){
+	var min_delay_flights = Number.MAX_SAFE_INTEGER;
+	var max_delay_flights = 0;
+	var airportSelectionData = [{id:"", text:"Select"}]; // data for airport selection
+	for (airport of data){
+		var delay_flights = parseInt(airport.DEP_DEL15);
+		if (delay_flights < min_delay_flights){
+			min_delay_flights = delay_flights;
+		}
+		if (delay_flights > max_delay_flights){
+			max_delay_flights = delay_flights;
+		}
+
+		// push data to airport selection
+		airportSelectionData.push({id: airport.ORIGIN, text: airport.origin_city + ", " + airport.ORIGIN});
+	}
+	var avg_delay_flights = (max_delay_flights - min_delay_flights)/2;
+   	
+	// load data for airport selection
+	$("#airportSelection").select2({data: airportSelectionData});
+
+	airportGraph.selectAll("circle")
+		.data(data)
+		.enter()
+		.append("circle")
+		.attr('cx', function (item) { return getProjectedPoint(projection, item)[0]; })
+		.attr('cy', function (item) { return getProjectedPoint(projection, item)[1]; })
+		.attr('name', function (item) { return item.ORIGIN; })
+		.attr('r', function (item) {return getAirportLevel(parseInt(item.DEP_DEL15), avg_delay_flights); })
+		.attr('class', 'airport')
+		.on("click", function(item){ airportOnClick(routeGraph, projection, item);})
+		.on("mouseover", function(item){ displayTooltip(item); })
+		.on("mouseout", function(item){ hideTooltip() });
+});
+
 function getProjectedPoint(projector, airport){
 	projectedPoint = projector([airport.origin_long, airport.origin_lat]);
 	if (projectedPoint == null){
@@ -137,33 +181,3 @@ function getAirportLevel(delay_flights, avg_delay_flights){
 	}
 	return "3px";
 }
-
-var airportGraph = svg.append("g");
-var routeGraph = svg.append("g");
-d3.csv("http://127.0.0.1:5000/static/origin_delay.csv").then(function(data){
-	var min_delay_flights = Number.MAX_SAFE_INTEGER;
-	var max_delay_flights = 0;
-	for (airport of data){
-		var delay_flights = parseInt(airport.DEP_DEL15);
-		if (delay_flights < min_delay_flights){
-			min_delay_flights = delay_flights;
-		}
-		if (delay_flights > max_delay_flights){
-			max_delay_flights = delay_flights;
-		}
-	}
-	var avg_delay_flights = (max_delay_flights - min_delay_flights)/2;
-	
-	airportGraph.selectAll("circleOrigin")
-		.data(data)
-		.enter()
-		.append("circle")
-		.attr('cx', function (item) { return getProjectedPoint(projection, item)[0]; })
-		.attr('cy', function (item) { return getProjectedPoint(projection, item)[1]; })
-		.attr('name', function (item) { return item.ORIGIN; })
-		.attr('r', function (item) {return getAirportLevel(parseInt(item.DEP_DEL15), avg_delay_flights); })
-		.attr('class', 'airport')
-		.on("click", function(item){ airportOnClick(routeGraph, projection, item);})
-		.on("mouseover", function(item){ displayTooltip(item); })
-		.on("mouseout", function(item){ hideTooltip() });
-});
