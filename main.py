36a9f6ceb045fd3fc7c793731d2a7af61837627b
dang <nguyenhaidang.uit@gmail.com>
2019-08-12 11:10:08 +0700

    update page link and restyle for map route

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return render_template("index.html")
+	return render_template("visualization.html")
 
 @app.route("/visualization",methods=["GET", "POST"])
 def visualize():
